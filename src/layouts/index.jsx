be4b74537f422f4e2140e86abb0262e7442e82c2
cbrwizard <cbrwizard@gmail.com>
2018-03-20 22:05:11 +0000

    Adds google & yandex web verification tags

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -10,6 +10,8 @@ class Layout extends React.Component {
       <div className="layout">
         <Helmet defaultTitle="Закат">
           <html lang="ru" />
+          <meta name="yandex-verification" content="5b5ac39133f16a29" />
+          <meta name="google-site-verification" content="nA2CqNDvZBVM828oWww_Gh-Ntvd3JBioqDgjXwG_3u4" />
         </Helmet>
         {children()}
       </div>
